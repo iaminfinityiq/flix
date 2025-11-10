c0da9225bc6d43fc75e89ecc54767994e2aab187
iaminfinityiq <133767092+iaminfinityiq@users.noreply.github.com>
2025-11-10 22:32:39 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext import commands, tasks
+from dot_env import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+general_id = int(os.getenv("GENERAL_ID"))
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    if not bot_loop.is_running():
+        bot_loop.start()
+
+@bot.event
+async def on_member_join(member):
+    general = bot.get_channel(general_id)
+    general.send(f"Greetings, {member.mention}! Welcome to Fluentix. Come here and chat with us")
+
+@tasks.loop(seconds=1)
+async def bot_loop():
+    pass
+
+@bot_loop.error
+async def bot_loop_error(error):
+    channel = bot.get_channel(general)
+    await channel.send(f"❌ Bot loop error: {error}")
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    await bot.process_commands(message)
+
+@bot.command()
+async def help(ctx):
+    await ctx.send("!help is in maintainence, cannot use for now 💀💀💀☠️☠️☠️")
